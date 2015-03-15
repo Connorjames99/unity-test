09e5e09cbd2a4c9371427b267ca4c2e25e64a841
Connor McCluskey <0033147@student.jwacs.wa.edu.au>
2015-03-15 16:10:56 +0800

    Initial Commit

---
+++ Test/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Test : MonoBehaviour
+{
+	[Serializable]
+	public class SpawnPoint
+	{
+		public int Id;
+		public Vector3 Position;
+		public int[] Nodes;
+
+		public SpawnPoint (int id, Vector3 position)
+		{
+			Id = id;
+			Position = position;
+		}
+	}
+
+	[SerializeField] private SpawnPoint m_SpawnPoint;
+
+	private void Start ()
+	{
+		m_SpawnPoint = new SpawnPoint (1, new Vector3 (1.1f, 1.0f, 2.3f));
+	}
+}
